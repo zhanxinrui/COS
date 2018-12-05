fdb2e290c1f2116c3c7bceac1cc737407821eacc
zhanxinrui <756222765@qq.com>
2018-12-05 18:23:22 +0700

    nothing

---
+++ src/Util/dateUtil.java
@@ -0,0 +1,30 @@
+package util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/*
+* 定义了一些时间转换的方便函数
+*
+* */
+
+
+public  class dateUtil {
+    static SimpleDateFormat sdf = new SimpleDateFormat("yyMMddHHmm");
+
+    /*
+    * 字符串转Date对象
+    * */
+    public static Date strToDate(String s) throws ParseException {
+        return sdf.parse(s);
+
+    }
+    /*
+    * 日期转字符串
+    * */
+    public static String dateToStr(Date d) throws ParseException {
+        return  sdf.format(d);
+    }
+
+}
