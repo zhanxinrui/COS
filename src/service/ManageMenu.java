abd9b4cf2c514b59a75258a6284132d657405949
ysdlb <libertyhan@outlook.com>
2018-12-06 22:35:41 +0800

    hgqhhh

--- src/service/ManageMenu.java
+++ src/service/ManageMenu.java
@@ -10,23 +10,19 @@ public class ManageMenu{
 	public static boolean addMenu(ResultSet rs, Food food){
 		String[] column_map = null;
 		Object[] column_value = null;
-		if(food.getPic() == null){
-			column_map = new String[]{"id", "name", "remain", "pic", "price", "special"};
-			column_value = new Object[]{
-				food.getId(), food.getName(), food.getRemain(), food.getPic(), food.getPrice(), food.getSpecial()
-			};
-		}
-		else{
-			column_map = new String[]{"id", "name", "remain", "pic", "price", "special"};
-			column_value = new Object[]{
-				food.getId(), food.getName(), food.getRemain(), food.getPic(), food.getPrice(), food.getSpecial()
-			};
-		}
+		column_map = new String[]{"id", "name", "remain", "pic", "price", "feature"};
+		column_value = new Object[]{
+			food.getId(), food.getName(), food.getRemain(), food.getPic(), food.getPrice(), food.getSpecial()
+		};
 		if(UpdatableSQL.insert(rs, column_map, column_value))
 			return true;
 		return false;
 	}
 
+	/**
+	 * 参数虽然为food, 但唯一标志表中某一项的是id
+	 * 数据库中对应id的food将会被删除
+	 */
 	public static boolean deleteMenu(ResultSet rs, Food food){
 		if(UpdatableSQL.delete(rs, "id", food.getId()))
 			return true;
@@ -41,19 +37,10 @@ public class ManageMenu{
 		UpdatableSQL up_sql = new UpdatableSQL("food");
 		String[] column_map = null;
 		Object[] column_value = null;
-		if(food.getPic() == null){
-			column_map = new String[]{"id", "name", "remain", "price", "special"};
+			column_map = new String[]{"id", "name", "remain", "price", "feature"};
 			column_value = new Object[]{
 				food.getId(), food.getName(), food.getRemain(), food.getPrice(), food.getSpecial()
 			};
-		}
-		else{
-			column_map = new String[]{"id", "name", "remain", "pic", "price", "special"};
-			column_value = new Object[]{
-				food.getId(), food.getName(), food.getRemain(), food.getPic(), food.getPrice(), food.getSpecial()
-			};
-		}
-
 		// 这类方法可以写成静态的,谁有时间改了在这儿做个标记
 		if(up_sql.updateToTable(column_map, column_value, "id", food.getId()))
 			return true;
